af53d345fdcde52c75fd8cce29514c5244b514f6
Anshu Yadav <anshu78358@gmail.com>
2023-09-10 16:06:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let audio = document.createElement("audio")
+audio.setAttribute("src", "Dhoom machale.mp3")
+audio.loop = true;
+audio.play()
+
+let audio2 = document.createElement("audio")
+audio2.setAttribute("src", "carSound.mp3")
+audio2.loop = true;
+audio2.play()
